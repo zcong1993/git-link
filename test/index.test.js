cf30698fa96b03b0d62a21a9266bf59af2f83b04
zcong1993 <1005997919@qq.com>
2017-04-25 10:56:44 +0800

    update test 🇸🇾

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,17 @@
 const gitLink = require('../')
 
-test('main', () => {
-  const samples1 = 'zcong1993/git-link'
-  const samples2 = 'zcong1993'
+const samples1 = 'zcong1993/git-link'
+const samples2 = 'zcong1993'
+
+it(`should work well with ${samples1}`, () => {
   expect(gitLink(samples1)).toEqual([
     ['github', `git@github.com:zcong1993/git-link.git`],
     ['https', `https://github.com/zcong1993/git-link.git`],
     ['gh-pages', `https://zcong1993.github.io/git-link`],
     ['gitbook', `https://git.gitbook.com/zcong1993/git-link.git`]
   ])
+})
+
+it(`should throw error with ${samples2}`, () => {
   expect(() => gitLink(samples2)).toThrow('Invalidate link!')
 })
